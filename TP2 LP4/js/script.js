273d23c336a72831a3fb3dd3b60a494bbc3636f3
Aluno <aluno@ceetepspg.local>
2017-11-17 21:01:57 -0200

    Atualização com os arquivos do vinicius

--- TP2 LP4/js/script.js
+++ TP2 LP4/js/script.js
@@ -1,15 +1,26 @@
+ $("#primeiro-membro").fadeTo('slow', 1, function() {
+                    $("#segundo-membro").fadeTo('slow', 1, function() {
+                        $("#terceiro-membro").fadeTo('slow', 1, function() {
+                            $("#quarto-membro").fadeTo('slow', 1, function() {
+                                
+                            }); 
+                        }); 
+                    });  
+                }); 
+            
               $(window).on('scroll', function() {
 
                   var y_scroll_pos = window.pageYOffset;
+                  
                   var scroll_pos_test = 150;
 
                   if(y_scroll_pos > scroll_pos_test) {
 
                   $("#titulo-beneficios").removeClass('hidden');
-
-
+               
+                
                   $("#titulo-beneficios").addClass('animated fadeInUp');
-
+                
 
                  scroll_pos_test = 190;
 
@@ -23,7 +34,7 @@
                   $("#txt-ben1").addClass('animated fadeInUp');
                   $("#txt-ben12").addClass('animated fadeInUp');
                   $("#txt-ben13").addClass('animated fadeInUp');}
-
+                 
 
                  scroll_pos_test = 700;
 
@@ -37,13 +48,13 @@
                   $("#txt-ben2").addClass('animated fadeInUp')
                   $("#txt-ben22").addClass('animated fadeInUp')
                   $("#txt-ben23").addClass('animated fadeInUp');}
-
+                 
 
                  scroll_pos_test = 1150;
 
                  if(y_scroll_pos > scroll_pos_test) {
                   $("#ben3").removeClass('hidden');
-
+                 
                   $("#txt-ben32").removeClass('hidden');
                   $("#txt-ben33").removeClass('hidden');
                    $("#txt-ben3").removeClass('hidden');
@@ -53,10 +64,12 @@
                   $("#txt-ben32").addClass('animated fadeInUp');
                   $("#txt-ben33").addClass('animated fadeInUp');}
 
-                  }
+                  }   
               });
 
               $('#myModal').on('shown.bs.modal', function () {
                 $('#myInput').focus()
               })
 
+             
+             
