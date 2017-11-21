a10f212a62e2973197b4635553fb8e758ef5123e
AdalbertoRodrigues <31300400+AdalbertoRodrigues@users.noreply.github.com>
2017-11-21 21:57:10 -0200

    Fragmentos para as páginas

---
+++ TP2 LP4/includes/modal_recuperar_senha.php
@@ -0,0 +1,30 @@
+        <div class="modal-dialog cascading-modal" role="document">
+           
+            <div class="modal-content">
+    
+               
+                <div class="modal-header danger-color white-text">
+                    <h4 class="title"><i class="fa fa-envelope"></i> Recuperar senha</h4>
+                    <button type="button" class="close waves-effect waves-light" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                
+                <div class="modal-body mb-0">
+    
+                    <div class="md-form form-sm">
+                        <i class="fa fa-envelope prefix"></i>
+                        <input type="email" id="recuperarSenha" class="form-control">
+                        <label for="recuperarSenha">Seu email</label>
+                    </div>
+    
+                        
+                    <div class="text-center mt-1-half">
+                        <button class="btn btn-success mb-1">EXCLUIR <i class="fa fa-check ml-1"></i></button>
+                    </div>
+    
+                </div>
+            </div>
+
+</div>
+</div>
