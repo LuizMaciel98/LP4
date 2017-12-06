ff206022efc486215a3b370fbde12fcc78a3973f
ViniciusLima5 <vinicius.lalmeida98@gmail.com>
2017-12-06 12:29:33 -0200

    Adição de mapa do google nos footers

--- TP2 LP4/includes/footer.php
+++ TP2 LP4/includes/footer.php
@@ -1,7 +1,7 @@
 	<footer style="margin-top: 0;" class="page-footer  cyan darken-4 center-on-small-only">
     <div class="container-fluid">
         <div class="row">          
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <h5 class="title">Siga a SOLARI nas redes sociais: </h5>
                 <div class="social">
               		<a class="fa fa-facebook " target="_blank" href="https://www.facebook.com/"></a>
@@ -10,21 +10,26 @@
               		<a class="fa fa-google-plus" target="_blank" href="https://www.google.com/"></a>
            		</div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <h5 class="title">Links Úteis</h5>
                 <ul>
                     <li><a href="http://www.fatecpg.edu.br/" target="_blank">FATEC-PG</a></li>
                     <li><a href="#!">TRABALHE CONOSCO</a></li>
                     <li><a data-toggle="modal" data-target="#contatoModal" href="#!">CONTATO</a></li>
                 </ul>
-            </div>      
-        </div>
+            </div>
+            <div class="col-md-4 centered-col">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.7361525882084!2d-46.4124712!3d-24.0050925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1db2e586da8d%3A0x271ae3e10bdc671e!2sFATEC!5e0!3m2!1sen!2sbr!4v1512569521543" width="400" height="100" frameborder="0" style="border:0; margin: 0; padding: 0;" allowfullscreen></iframe>      
+             </div>
+
+            </div>
     </div>
     <div class="footer-copyright">
         <div class="container-fluid">
             © 2017 Copyright: <a href="https://www.facebook.com/vinicius.lima.1671" target="_blank"> Vinícius Lima </a>
         </div>
     </div>
+
 	</footer>
 
 	
--- TP2 LP4/includes/footer2.php
+++ TP2 LP4/includes/footer2.php
@@ -1,7 +1,7 @@
 <footer style="margin-top: 0; color: black;" class="footer page-footer  amber darken-3 center-on-small-only">
     <div class="container-fluid">
         <div class="row">          
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <h5 class="title">Siga a SOLARI nas redes sociais: </h5>
                 <div style="color: black;" class="social">
                     <a class="fa fa-facebook " target="_blank" href="https://www.facebook.com/"></a>
@@ -10,14 +10,17 @@
                     <a class="fa fa-google-plus" target="_blank" href="https://www.google.com/"></a>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <h5 class="title">Links Úteis</h5>
                 <ul>
                     <li><a style="color: #006064;" href="http://www.fatecpg.edu.br/" target="_blank">FATEC-PG</a></li>
                     <li><a style="color: #006064;" href="#!">TRABALHE CONOSCO</a></li>
                     <li><a style="color: #006064;" data-toggle="modal" data-target="#contatoModal" href="#!">CONTATO</a></li>
                 </ul>
-            </div>      
+            </div>
+            <div class="col-md-4 centered-col">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.7361525882084!2d-46.4124712!3d-24.0050925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1db2e586da8d%3A0x271ae3e10bdc671e!2sFATEC!5e0!3m2!1sen!2sbr!4v1512569521543" width="400" height="100" frameborder="0" style="border:0; margin: 0; padding: 0;" allowfullscreen></iframe>      
+             </div>    
         </div>
     </div>
     <div class="footer-copyright">
